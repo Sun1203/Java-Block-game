6f1e93851f39b25e9f4eee9e7095f3a15072f808
Sun1203 <jasun1208@naver.com>
2022-09-09 18:03:49 +0900

    블록 게임 최종.

--- Blockgame/src/Blockgame.java
+++ Blockgame/src/Blockgame.java
@@ -26,13 +26,14 @@ public class Blockgame {
 		// variable
 		static MyPanel myPanel = null;
 		static int score = 0;
-		static Timer time = null;
+		static Timer timer = null;
 		static Block[][] blocks = new Block[BLOCK_ROWS][BLOCK_COLUMNS];
 		static Bar bar = new Bar();
 		static Ball ball = new Ball();
 		static int barXTarget = bar.x; //Target Value - interpolation
 		static int dir = 0; //0 : Up-Right 1 : Down-Right 2 : Up-Left 3 : Down-Left
 		static int ballSpeed = 5;
+		static boolean isGameFinish = false;
 		
 		
 		
@@ -41,6 +42,25 @@ public class Blockgame {
 			int y = CANVAS_HEIGHT / 2 - BALL_HEIGHT / 2;
 			int width = BALL_WIDTH;
 			int height = BALL_HEIGHT;
+			
+			Point getCenter() {
+				return new Point( x + (BALL_WIDTH / 2), y + (BALL_HEIGHT / 2) );
+			}
+			Point getBottomCenter() { 
+				return new Point( x + (BALL_WIDTH / 2), y + (BALL_HEIGHT) );
+			}
+			
+			Point getTopCenter() { 
+				return new Point( x + (BALL_WIDTH / 2), y);
+			}
+			
+			Point getLeftCenter() { 
+				return new Point( x , y + (BALL_HEIGHT / 2) );
+			}
+			
+			Point getRightCenter() { 
+				return new Point( x + (BALL_WIDTH), y + (BALL_HEIGHT/2));
+			}
 		}
 		
 		static class Bar {
@@ -59,7 +79,64 @@ public class Blockgame {
 			boolean isHidden = false; // 충돌후 사라지게 한다. 
 		}
 		
-		static class MyPanel extends JPanel {
+		static class MyPanel extends JPanel { //CANVAS for Draw!
+			public MyPanel() {
+				this.setSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+				this.setBackground(Color.BLACK);
+			}
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				Graphics2D g2d = (Graphics2D)g;
+				
+				drawUI( g2d );
+			}
+			private void drawUI(Graphics2D g2d) {
+				// draw Blocks
+				for (int i=0;i<BLOCK_ROWS;i++) {
+					for (int j=0;j<BLOCK_COLUMNS;j++) {
+						if (blocks[i][j].isHidden) {
+							continue;
+						}
+						if (blocks[i][j].color == 0) {
+							g2d.setColor(Color.WHITE);
+						}
+						else if (blocks[i][j].color == 1) {
+							g2d.setColor(Color.YELLOW);
+						}
+						else if (blocks[i][j].color == 2) {
+							g2d.setColor(Color.BLUE);
+						}
+						else if (blocks[i][j].color == 3) {
+							g2d.setColor(Color.MAGENTA);
+						}
+						else if (blocks[i][j].color == 4) {
+							g2d.setColor(Color.RED);
+						}
+						
+						g2d.fillRect(blocks[i][j].x, blocks[i][j].y, blocks[i][j].width, blocks[i][j].height);
+					}
+					
+					//draw score
+					g2d.setColor(Color.WHITE);
+					g2d.setFont(new Font("TimesRoman", Font.BOLD, 20));
+					g2d.drawString("score : " + score, CANVAS_WIDTH/2 - 30, 20);
+					if (isGameFinish) {
+						g2d.setColor(Color.RED);
+						g2d.drawString("Game Finish!", CANVAS_WIDTH/2 - 55, 50);
+					}
+					
+					//draw ball
+					g2d.setColor(Color.WHITE);
+					g2d.fillOval(ball.x, ball.y, BALL_WIDTH, BALL_HEIGHT);
+					
+					//draw Bar
+					g2d.setColor(Color.WHITE);
+					g2d.fillRect(bar.x, bar.y, bar.width, bar.height);
+
+				}
+				
+			}
 			
 		}
 		
@@ -80,15 +157,242 @@ public class Blockgame {
 		}
 		
 		public void initData() {
-			
+			for (int i=0;i<BLOCK_ROWS;i++) {
+				for (int j=0;j<BLOCK_COLUMNS;j++) {
+					blocks[i][j] = new Block();
+					blocks[i][j].x = BLOCK_WIDTH*j + BLOCK_GAP*j;
+					blocks[i][j].y = 100 + i*BLOCK_HEIGHT + BLOCK_GAP*i;
+					blocks[i][j].width = BLOCK_WIDTH;
+					blocks[i][j].height = BLOCK_HEIGHT;
+					blocks[i][j].color = 4 - i; //  0 :white 1 : yellow 2:blue 3:mazanta 4:
+					blocks[i][j].isHidden = false;
+				}
+			}
 		}
 		
 		public void setKeyListener() {
-			
+			this.addKeyListener( new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) { // Key Event : 컴퓨터에서 키보드값 입력받
+					if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+						System.out.println("Pressed Left Key");
+						barXTarget -= 20;
+						if ( bar.x < barXTarget ) { // 계속 키를 눌렀을때. 
+							barXTarget = bar.x;
+						}
+					}
+					else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+						System.out.println("Pressed Right Key");
+						barXTarget += 20;
+						if ( bar.x > barXTarget ) { // 계속 키를 눌렀을때. 
+							barXTarget = bar.x;
+						}
+					}
+				}
+			});
 		}
 		
 		public void startTimer() {
+			timer = new Timer(20, new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) { // Timer Event
+					movement();
+					checkCollision(); //Wall, Bar
+					checkCollisionBlock(); // Blocks 50
+					myPanel.repaint(); // Redraw
+					
+					isGameFinish();
+				}
+			});
+			timer.start(); // Start Timer!
+		}
+		
+		public void isGameFinish() {
+			// Game success
+			int count = 0;
+			for (int i=0;i<BLOCK_ROWS;i++) {
+				for (int j=0;j<BLOCK_COLUMNS;j++) {
+					Block block = blocks[i][j];
+					if ( block.isHidden ) count++;
+				}
+			}
+			if (count == BLOCK_ROWS * BLOCK_COLUMNS) {
+				// Game Finished!
+				isGameFinish = true;
+				
+			}
+		}
+		
+		public void movement() {
+			if (bar.x < barXTarget) { 
+				bar.x += 5;
+			}
+			else if (bar.x > barXTarget ) {
+				bar.x -= 5;
+			}
 			
+			if (dir == 0) { // 0 : Up-Right
+				ball.x += ballSpeed;
+				ball.y -= ballSpeed;
+			} else if (dir == 1) { // 1 : Down-Right
+				ball.x += ballSpeed;
+				ball.y += ballSpeed;
+			} else if (dir == 2) { // 2 : Up-Left
+				ball.x -= ballSpeed;
+				ball.y -= ballSpeed;
+			}else if (dir == 3) { // 3 : Down-Left
+				ball.x -= ballSpeed;
+				ball.y += ballSpeed;
+			}
+		}
+		public  boolean duplRect(Rectangle rect1, Rectangle rect2) {
+			return rect1.intersects(rect2); // check two Rect is Duplicated! 서로 영역이 중복되는지 확인.
+		}
+		public void checkCollision() {
+			if (dir == 0) { // 0 : Up-Right
+				// Wall
+				if (ball.y < 0) dir = 1; // wall upper
+				if (ball.x > CANVAS_WIDTH - BALL_WIDTH) dir = 2; // wall right
+				//Bar
+				//none
+				
+			} else if (dir == 1) { // 1 : Down-Right
+				// Wall
+				if (ball.y > CANVAS_HEIGHT - BALL_HEIGHT-BALL_HEIGHT) {
+					dir = 0; // wall bottom
+					
+					// game reset
+					dir = 0;
+					ball.x = CANVAS_WIDTH/2 - BALL_WIDTH/2;
+					ball.y = CANVAS_HEIGHT/2 - BALL_HEIGHT/2;
+					score = 0;
+				}
+				if (ball.x > CANVAS_WIDTH - BALL_WIDTH) dir = 3; // wall left
+				//Bar
+				if ( ball.getBottomCenter().y >= bar.y ) {
+					if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+							new Rectangle (bar.x, bar.y, bar.width, bar.height))) dir = 0;
+				}
+			} else if (dir == 2) { // 2 : Up-Left
+				// Wall
+				if (ball.y < 0) dir = 3; // wall upper
+				if (ball.x < 0) dir = 0; // wall left
+				//Bar
+				// none
+			}else if (dir == 3) { // 3 : Down-Left
+				if (ball.y > CANVAS_HEIGHT - BALL_HEIGHT - BALL_HEIGHT) {
+					dir = 2; // wall bottom
+					
+					// game reset
+					dir = 0;
+					ball.x = CANVAS_WIDTH/2 - BALL_WIDTH/2;
+					ball.y = CANVAS_HEIGHT/2 - BALL_HEIGHT/2;
+					score = 0;
+				}
+				if (ball.x < 0) dir = 1;
+				// Bar
+				if ( ball.getBottomCenter().y >= bar.y ) {
+					if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+							new Rectangle (bar.x, bar.y, bar.width, bar.height))) dir = 2;
+				}
+			}
+			
+		}
+		
+		public void checkCollisionBlock() {
+			//0 : Up-Right 1 : Down-Right 2 : Up-Left 3 : Down-Left
+			for (int i=0;i<BLOCK_ROWS;i++) {
+				for (int j=0;j<BLOCK_COLUMNS;j++) {
+					Block block = blocks[i][j];
+					if (block.isHidden == false) {
+						System.out.println("test");
+						if (dir ==0) { //  Up-Right	
+							if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+									new Rectangle (block.x, block.y, block.width, block.height))) {
+								if (ball.x > block.x + 2 && 
+									ball.getRightCenter().x <= block.x + block.width - 2) {
+									// block bottom collision
+									dir = 1;
+								}
+								else {
+									// block left collision
+									dir = 2;
+								}
+								block.isHidden = true;
+								if (block.color == 0) score += 10;
+								else if (block.color == 1) score += 10;
+								else if (block.color == 2) score += 10;
+								else if (block.color == 3) score += 10;
+								else if (block.color == 4) score += 10;
+							}
+							
+						} 
+						else if (dir == 1) { // 1 : Down-Right
+							
+							if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+									new Rectangle (block.x, block.y, block.width, block.height))) {
+								if (ball.x > block.x + 2 && 
+									ball.getRightCenter().x <= block.x + block.width - 2) {
+									// block top collision
+									dir = 0;
+								}
+								else {
+									// block left collision
+									dir = 3;
+								}
+								block.isHidden = true;
+								if (block.color == 0) score += 10;
+								else if (block.color == 1) score += 10;
+								else if (block.color == 2) score += 10;
+								else if (block.color == 3) score += 10;
+								else if (block.color == 4) score += 10;
+							}
+						} 
+						else if (dir == 2) { // 2 : Up-Left 
+							
+							if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+									new Rectangle (block.x, block.y, block.width, block.height))) {
+								if (ball.x > block.x + 2 && 
+									ball.getRightCenter().x <= block.x + block.width - 2) {
+									// block bottom collision
+									dir = 3;
+								}
+								else {
+									// block right collision
+									dir = 0;
+								}
+								block.isHidden = true;
+								if (block.color == 0) score += 10;
+								else if (block.color == 1) score += 10;
+								else if (block.color == 2) score += 10;
+								else if (block.color == 3) score += 10;
+								else if (block.color == 4) score += 10;
+							}
+							
+						} 
+						else if (dir == 3) { // 3 : Down-Left
+							if ( duplRect (new Rectangle(ball.x, ball.y, ball.width, ball.height) , 
+									new Rectangle (block.x, block.y, block.width, block.height))) {
+								if (ball.x > block.x + 2 && 
+									ball.getRightCenter().x <= block.x + block.width - 2) {
+									// block top collision
+									dir = 2;
+								}
+								else {
+									// block right collision
+									dir = 1;
+								}
+								block.isHidden = true;
+								if (block.color == 0) score += 10;
+								else if (block.color == 1) score += 10;
+								else if (block.color == 2) score += 10;
+								else if (block.color == 3) score += 10;
+								else if (block.color == 4) score += 10;
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
